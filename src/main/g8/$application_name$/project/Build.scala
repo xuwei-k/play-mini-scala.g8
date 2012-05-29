2315156e2430e2f0090649b76d27377bc2a60053
peter hausel <peter_kovac11@hotmail.com>
2012-05-28 23:14:03 -0400

    exclude spring, configure assembly

--- src/main/g8/$application_name$/project/Build.scala
+++ src/main/g8/$application_name$/project/Build.scala
@@ -9,14 +9,16 @@ object MinimalBuild extends Build {
   
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
-
-  lazy val root = Project(id = "$application_name$", base = file("."), settings = Project.defaultSettings).settings(
+  val netty = Some("play.core.server.NettyServer") 
+  lazy val root = Project(id = "$application_name$", base = file("."), settings = Project.defaultSettings ++ assemblySettings).settings(
     version := buildVersion,
     organization := "$organization$",
     resolvers += typesafe,
     resolvers += typesafeSnapshot,
     logManager <<= extraLoggers(com.typesafe.util.Sbt.logger),
     libraryDependencies += "com.typesafe" %% "play-mini" % buildVersion,
-    mainClass in (Compile, run) := Some("play.core.server.NettyServer")
-  ).settings(assemblySettings: _*)
+    mainClass in (Compile, run) := netty,
+    mainClass in assembly := netty,
+    ivyXML := <dependencies> <exclude org="org.springframework"/> </dependencies>
+  )
 }
