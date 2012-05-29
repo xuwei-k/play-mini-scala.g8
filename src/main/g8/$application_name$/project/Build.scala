e021599e952fe6e10ae6dc69883be52c543ec059
peter hausel <peter_kovac11@hotmail.com>
2012-05-29 16:26:45 -0400

    sanitise build script

--- src/main/g8/$application_name$/project/Build.scala
+++ src/main/g8/$application_name$/project/Build.scala
@@ -3,20 +3,31 @@ import Keys._
 import sbtassembly.Plugin._
 import AssemblyKeys._
 
-object MinimalBuild extends Build {
+object MinimalBuild extends Build with ConfigureScalaBuild{
 
-  lazy val buildVersion =  "$play_mini_version$"
+  lazy val root = scalaMiniProject("$organization$","$application_name$","$application_version$").settings(
+     //custom settings come here ie 
+     //libraryDependencies += "organization" %% "group.id" % "version"
+    )
+  
+}
+
+trait ConfigureScalaBuild {
+
   
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+  
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
+  
   val netty = Some("play.core.server.NettyServer") 
-  lazy val root = Project(id = "$application_name$", base = file("."), settings = Project.defaultSettings ++ assemblySettings).settings(
+
+  def scalaMiniProject(org: String, name: String, buildVersion: String, baseFile: java.io.File = file(".")) = Project(id = name, base = baseFile, settings = Project.defaultSettings ++ assemblySettings).settings(
     version := buildVersion,
-    organization := "$organization$",
+    organization := org,
     resolvers += typesafe,
     resolvers += typesafeSnapshot,
     logManager <<= extraLoggers(com.typesafe.util.Sbt.logger),
-    libraryDependencies += "com.typesafe" %% "play-mini" % buildVersion,
+    libraryDependencies += "com.typesafe" %% "play-mini" % "$play_mini_version$",
     mainClass in (Compile, run) := netty,
     mainClass in assembly := netty,
     ivyXML := <dependencies> <exclude org="org.springframework"/> </dependencies>
